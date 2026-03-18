a6b4c5ab95601c825754a9b503c5d50b2b666670
RobbyDR <rdrrdr87@gmail.com>
2026-03-18 17:11:26 +0700

    apply selective gitignore

--- BATCH3/tflite.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-interpreter = tf.lite.Interpreter(model_path="model.tflite")
-interpreter.allocate_tensors()
-
-ops = interpreter._get_ops_details()
-used_ops = set([op['op_name'] for op in ops])
-print(used_ops)
